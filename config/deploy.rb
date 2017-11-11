165fb23b5752142bce60c2e19d965339a9cde12d
Juan Salvador Perez Garcia <jsperezg@gmail.com>
2017-11-11 08:52:41 +0100

    Deploy adjustments
    Added migration that enables postgres extension

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.3.5'
 
+set :application, 'decidim-staging'
 set :user, 'barcelonadecidim'
 set :repo_url, "https://github.com/aspgems/decidim-staging.git"
 
