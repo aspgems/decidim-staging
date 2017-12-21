341ed708a814e74f347d9524997ae78756075438
Juan Salvador Perez Garcia <jsperezg@gmail.com>
2017-12-21 11:52:05 +0100

    Decidim sortitions
    * Decidim sortitions has been updated accordingly to last P.O. comments.
    * Unnecessary development files have been removed from the repo.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171220093837) do
+ActiveRecord::Schema.define(version: 20171221104906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -409,6 +409,8 @@ ActiveRecord::Schema.define(version: 20171220093837) do
     t.jsonb "selected_proposals"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.jsonb "witnesses"
+    t.jsonb "additional_info"
     t.index ["decidim_category_id"], name: "index_sortitions__on_category"
     t.index ["decidim_feature_id"], name: "index_sortitions__on_feature"
     t.index ["decidim_proposals_feature_id"], name: "index_sortitions__on_proposals_feature"
