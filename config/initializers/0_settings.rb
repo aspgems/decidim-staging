32d36e5fd65cbc65e0e72edd1fb3f7a37322cf4c
Juan Salvador Perez Garcia <jsperezg@gmail.com>
2018-02-22 15:12:50 +0100

    Added custom settings for chamber

---
+++ config/initializers/0_settings.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Load application settings into the ::Settings class. See https://github.com/m5rk/chamber for details
+class Settings
+  extend Chamber
+
+  load files: [Rails.root + 'config/settings/*.{yml,yml.erb}'],
+       decryption_key: Rails.root + 'config/chamber.pem',
+       namespaces: { environment: ::Rails.env }
+
+  def self.env
+    super
+  end
+end
+
